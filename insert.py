8af89e8d32a1dbd1fc04373c3215bc4dc22ac70a
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2019-06-04 19:25:37 +0800

    Add county csv

--- insert.py
+++ insert.py
@@ -4,7 +4,7 @@ import MySQLdb as mdb
 conn = mdb.connect('localhost', 'root', 'root', 'chuchudb')
 cursor = conn.cursor()
 
-table_name = 'state'
+table_name = 'county'
 
 with open('data/'+table_name+'.csv', 'r') as state:
     csv = state.read().splitlines()
@@ -23,9 +23,11 @@ with open('data/'+table_name+'.csv', 'r') as state:
             insert_query = "insert into " + table_name + " (" + csv_header.replace('"', '') + ")"
             values = " values (" + csv[i] + ")"
             sql = insert_query + values
+            # print(sql)
             cursor.execute('SET FOREIGN_KEY_CHECKS = 0;')
             cursor.execute(sql)
 
 cursor.close()
 conn.commit()
 conn.close()
+print("Done migrating "+table_name)
