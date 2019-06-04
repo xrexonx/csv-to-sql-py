6c46000d6b8d509f5f3a034e92f97499702eb072
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2019-06-04 10:50:16 +0800

    Add virtualenv

--- insert.py
+++ insert.py
@@ -3,14 +3,16 @@ import MySQLdb as mdb
 conn = mdb.connect('localhost', 'root', 'root', 'chuchudb')
 cursor = conn.cursor()
 
-with open('data/state.csv', 'r') as dmas:
-    lines = dmas.read().splitlines()
+with open('data/states.csv', 'r') as state:
+    lines = state.read().splitlines()
     for i in range(1, len(lines)):
-        # _tuple = lines[i].split(',')
-        cursor.execute(
-            "SET FOREIGN_KEY_CHECKS = 0; INSERT INTO state (abbreviation, name, usps) VALUES ('%s', '%s', '%s');" %
-            ("HK", "Eighty 8", 0)
-        )
+        row = lines[i].split(',')
+        print(row)
+        # cursor.execute('SET FOREIGN_KEY_CHECKS = 0;')
+        # cursor.execute(
+        #     "INSERT INTO state (abbreviation, name, usps) VALUES ('%s', '%s', '%s');" %
+        #     ("HK", "Eighty 8", 0)
+        # )
 
 cursor.close()
 conn.commit()
