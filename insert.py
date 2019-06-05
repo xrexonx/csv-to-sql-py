8310411bafb5026d40490f56743be2dbcab1ca5b
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2019-06-05 17:39:30 +0800

    Fix virtual env

--- insert.py
+++ insert.py
@@ -1,28 +1,35 @@
-# import os
+import os
 import MySQLdb as mdb
+from dotenv import load_dotenv
+import emoji
 
 conn = mdb.connect('localhost', 'root', 'root', 'chuchudb')
 cursor = conn.cursor()
 
 table_name = 'county'
-
-with open('data/'+table_name+'.csv', 'r') as state:
-    csv = state.read().splitlines()
+load_dotenv()
+print(emoji.emojize('Python is :thumbs_up:'))
+with open('data/'+table_name+'.csv', 'r') as csv_file:
+    csv = csv_file.read().splitlines()
     csv_header = csv[0]
-    cursor.execute('SELECT * FROM '+table_name+';')
-    # db_name = os.getenv("DB_NAME")
-    # print(db_name)
+    cursor.execute('select * from '+table_name+';')
+    db_name = os.getenv("DB_NAME")
+    print(db_name)
     # print(os.listdir("data"))
-
     # for x in os.listdir('data'):
     #     print(x)
+
+    def nullify(csv_row):
+        return list(map(lambda row: '0' if row == "" else row, csv_row.split(',')))
 
     # Check if done migration
     if not cursor.fetchone():
         for i in range(1, len(csv)):
             insert_query = "insert into " + table_name + " (" + csv_header.replace('"', '') + ")"
-            values = " values (" + csv[i] + ")"
-            sql = insert_query + values
+            csv_data = ', '.join(nullify(csv[i]))
+            values = " values (" + csv_data + ")"
+            sql = insert_query+values
+            # sql = mdb.escape_string(insert_query+values)
             cursor.execute('SET FOREIGN_KEY_CHECKS = 0;')
             cursor.execute(sql)
 
